366606c8929c63f6fd2802cd87763e98442ba656
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2021-10-20 13:01:49 +0530

    Index.js File Updated

--- index.js
+++ index.js
@@ -13,3 +13,45 @@ document.getElementById("backend").onclick = function (){
     document.getElementById("backend_img").style.display = "block";
     document.getElementById("frontend_img").style.display = "none";
 }
+
+// Image Slider Section Code
+
+// Slider Section Script
+
+var imageno = 1;
+displayimage(imageno);
+
+function plusSlides(n){
+
+    displayimage (imageno += n);
+}
+
+function currentSlides(n){
+
+    displayimage (imageno = n);
+}
+
+function displayimage(n){
+
+    var i;
+    var slideImage = document.getElementsByClassName("slideImage");
+    var dot = document.getElementsByClassName("dots");
+
+    if (n > slideImage.length){
+        imageno = 1;
+    }
+
+    if(n < 1){
+        imageno = slideImage.length;
+    }
+
+    for(i=0; i < slideImage.length; i++){
+        slideImage[i].style.display = "none"; 
+    }
+    for(i=0; i < dot.length; i++){
+        dot[i].className = dot[i].className.replace("active", " ");
+    }
+
+    slideImage[imageno  - 1].style.display = "block";
+    dot[imageno - 1].className += " active ";
+}
